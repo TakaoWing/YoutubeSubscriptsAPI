d0e277b9cbd06dc87c8f5d91a78924b26f3dcd4c
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2023-02-16 21:26:55 +0900

    チャンクごとに分割

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from youtube_transcript_api import YouTubeTranscriptApi
 from pytube import YouTube
+import json
 
 app = FastAPI()
 
@@ -13,11 +14,14 @@ def get_youtube_video(video_id: str):
         raise HTTPException(status_code=404, detail='Invalid video ID')
 
 
-def get_transcript(video_id: str):
+def get_transcript_chunks(video_id: str, chunk_size: int = 2500):
     try:
         transcript = YouTubeTranscriptApi.get_transcript(
             video_id, languages=['ja'])
-        return ' '.join([line['text'] for line in transcript])
+        text = ''.join([line['text'].strip() for line in transcript])
+        chunks = [text[i:i+chunk_size]
+                  for i in range(0, len(text), chunk_size)]
+        return [{'chunk': chunk} for chunk in chunks]
     except Exception:
         raise HTTPException(status_code=404, detail='Transcript not available')
 
@@ -25,10 +29,10 @@ def get_transcript(video_id: str):
 @app.get('/api/video/{video_id}')
 def get_video_info(video_id: str):
     video = get_youtube_video(video_id)
-    transcript = get_transcript(video_id)
+    transcript_chunks = get_transcript_chunks(video_id)
     url = 'https://youtu.be/'+video_id
     return {
         'title': video.title,
         'url': url,
-        'transcript': transcript
+        'transcript': transcript_chunks
     }
