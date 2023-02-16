2ba40909dde57dccfdf881960c89484979bc1da0
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2023-02-16 20:18:04 +0900

    更新

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def get_data():
     return data
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=8000)
